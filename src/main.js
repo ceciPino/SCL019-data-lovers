549c6ba417c001ab30b46febb75e0cf485937879
ceciPino <mepino@uc.cl>
2022-01-29 00:02:26 -0300

    trayendo data (o tratando)

--- src/main.js
+++ src/main.js
@@ -7,9 +7,15 @@ const dataghibli = data.films;
 console.log(dataghibli);
 
 for(let i=0;i<dataghibli.length;i++){
-    document.getElementById("root").innerHTML+=dataghibli[i].poster 
-    console.log(dataghibli[i].title)
-    console.log(dataghibli[i].director)
-    console.log(dataghibli[i].poster)
+   //document.getElementById("afiche").innerHTML+=dataghibli[i].poster;
+   let image = document.createElement("img");
+   let flexbox1 = document.createElement("flexbox");
+    image.setAttribute("src", dataghibli[i].poster);
+
+    afiche.appendChild(image);
+    image.appendChild(flexbox);
+
+    image.setAttribute("class", "cartel");
+   
 }
 
