46bf11f23858e7b3878a4c659e910da15620d976
amandamheredia <amanda.mnzh@gmail.com>
2022-02-01 10:45:20 -0300

    cambios js

--- src/main.js
+++ src/main.js
@@ -4,19 +4,26 @@ import data from './data/ghibli/ghibli.js';
 
 //TRAER DATA DE PELÍCULAS
 const dataghibli = data.films;
-console.log(dataghibli);
 const sectionAfiche = document.getElementById("root")
-
+// const containerPoster = document.getElementById("poster")
 const showFilms = (dataghibli) => {
+
 for(let i=0;i<dataghibli.length;i++){
-   //document.getElementById("afiche").innerHTML+=dataghibli[i].poster;
+
    let image = document.createElement("img");
-//    image.setAttribute("class", "cartel");
-   image.setAttribute("src", dataghibli[i].poster);
    let divImagen = document.createElement("divimg");
-//    let flexbox1 = document.createElement("flexbox");
-    divImagen.appendChild(image);
-    sectionAfiche.appendChild(divImagen);
+   let nombre = document.createElement("p")
+
+   image.setAttribute("src", dataghibli[i].poster);
+   nombre.setAttribute("id", "titulo")
+   nombre.innerHTML = dataghibli[i].title;
+
+   divImagen.appendChild(image);
+   sectionAfiche.appendChild(divImagen);
+//    containerPoster.appendChild(image);
+
+
+
 }
 };
 
