b4003d2f95118b3251c0cc404bc27bc8967edc37
Anabella Lincopan <anabella.lincopan@ug.uchile.cl>
2022-02-01 07:52:39 -0400

    se redondean imagenes y se fija el menu

--- src/main.js
+++ src/main.js
@@ -1,32 +1,30 @@
 import data from './data/ghibli/ghibli.js';
 
-console.log(data);
+// console.log(data);
 
+//TRAER DATA DE PELÍCULAS
 const dataghibli = data.films;
-
 console.log(dataghibli);
-
 const sectionAfiche = document.getElementById("root")
 
 const showFilms = (dataghibli) => {
-
 for(let i=0;i<dataghibli.length;i++){
    //document.getElementById("afiche").innerHTML+=dataghibli[i].poster;
    let image = document.createElement("img");
 //    image.setAttribute("class", "cartel");
    image.setAttribute("src", dataghibli[i].poster);
-   let divImagen = document.createElement("div");
+   let divImagen = document.createElement("divimg");
 //    let flexbox1 = document.createElement("flexbox");
-    
-
     divImagen.appendChild(image);
-    sectionAfiche.appendChild(divImagen);  
-   
+    sectionAfiche.appendChild(divImagen);
 }
 };
 
 document.getElementById("peliculas").addEventListener('click', () => {
     showFilms(dataghibli)
 });
+
+
+
 
 
