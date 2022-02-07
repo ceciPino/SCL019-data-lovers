95c3fbaab23e0188225ba8c7dd02fdfbbb75f519
Anabella Lincopan <anabella.lincopan@ug.uchile.cl>
2022-02-06 20:12:21 -0400

    funcion para filtrar peliculas segun sus directores

--- src/main.js
+++ src/main.js
@@ -1,61 +1,64 @@
-import data from './data/ghibli/ghibli.js';
-
-// console.log(data);
+// import { filterDirectors } from './data.js';
+import * as all from "./data.js";
+import data from "./data/ghibli/ghibli.js";
 
 //Acceder a data de las peliculas
 const dataghibli = data.films;
 
-//
-const sectionAfiche = document.getElementById("root")
+const sectionAfiche = document.getElementById("root");
 // const sectionDirectores = document.getElementById("dirContainer")
 // const persContainer = document.getElementById("persContainer");
 
+// PERSONAJES
 const charImage = (characters, name) => {
-return` 
+  return `
 <div>
 <h4 class="nombre-personaje">${name}</h4>
 <img src="${characters}">
 </div>`;
-
 };
 
 const showCharacters = (dataghibli) => {
-sectionAfiche.innerHTML= ""
-for (let i= 0; i <dataghibli.length; i++) {
- let characters = dataghibli[i].people;
-  for (let j=0; j <characters.length; j++) {
-      sectionAfiche.innerHTML += charImage(characters[j].img, characters[j].name);
-  }
-
+  sectionAfiche.innerHTML = "";
+  for (let i = 0; i < dataghibli.length; i++) {
+    let characters = dataghibli[i].people;
+    for (let j = 0; j < characters.length; j++) {
+      sectionAfiche.innerHTML += charImage(
+        characters[j].img,
+        characters[j].name
+      );
     }
+  }
 };
 
+//UFNCION PARA MOSTRAR PELICULAS
 const showFilms = (dataghibli) => {
-sectionAfiche.innerHTML= ""
-for(let i=0;i<dataghibli.length;i++){
+  sectionAfiche.innerHTML = "";
+  sectionAfiche.style.display = "grid";
+  // document.getElementById('filter-section').style.display='block';
+  for (let i = 0; i < dataghibli.length; i++) {
+    let image = document.createElement("img");
+    let divImagen = document.createElement("div");
+    let nombre = document.createElement("p");
 
-   let image = document.createElement("img");
-   let divImagen = document.createElement("div");
-   let nombre = document.createElement("p")
+    image.setAttribute("src", dataghibli[i].poster);
+    image.setAttribute("class", "imgfilm")
+    divImagen.setAttribute("class", "divimagen");
+    nombre.setAttribute("class", "titulo");
 
+    nombre.innerHTML = dataghibli[i].title;
 
-   image.setAttribute("src", dataghibli[i].poster, "class", "pelicula");
-   nombre.setAttribute("class", "titulo");
-   
-   nombre.innerHTML = dataghibli[i].title;
-
-   divImagen.appendChild(image);
-   sectionAfiche.appendChild(divImagen);
-   divImagen.appendChild(nombre);
-
-   
-}
+    divImagen.appendChild(image);
+    sectionAfiche.appendChild(divImagen);
+    divImagen.appendChild(nombre);
+    //    sectionAfiche.appendChild(filterandsort);
+  }
 };
 
+//FUNCION PARA MOSTRAR DIRECTORES
 const showDirectors = (dataghibli) => {
-sectionAfiche.innerHTML= ""
-for(let i=0;i<dataghibli.length;i++){
-
+  sectionAfiche.innerHTML = "";
+  for (let i = 0; i < dataghibli.length; i++) {
     let directors = document.createElement("p");
 
     directors.setAttribute("id", dataghibli[i].director);
@@ -68,35 +71,42 @@ for(let i=0;i<dataghibli.length;i++){
     /*let directorsFilt = directors.filter((item,index)=> {
         return directors.indexOf(item) === index;
     })*/
-
-}
+  }
 };
 
-document.getElementById("personajes").addEventListener('click', () => { 
-    showCharacters(dataghibli)
+// SECCIÓN FILTRAR
+const filterandsort = document.getElementsByClassName("filterandsort-section");
+
+// EL evento change se  usa con elementos "section", "input" y "textarea", su estructura es la sgte:
+const filterSection = document.getElementById("filter-section"); // se declara la variable asociada al fitro y se agrega el evento a esa variable
+filterSection.addEventListener("change", (e) => {
+  let filterItem = e.currentTarget.value; //se llama a los valores de los elementos asociados al evento
+  // console.log(filterItem)//
+  let directorsByFilms = all.filterDirectors(filterItem, dataghibli); //se llaman a todos los elementos dentro de la función creada en data.js
+  showFilms(directorsByFilms); // se muestran las peliculas asociadas a los directores 
+  sectionAfiche.style.display = "flex"; // se muestran las peliculas ordenadas en flex
 });
 
-document.getElementById("peliculas").addEventListener('click', () => {
-    showFilms(dataghibli)
+//SECCIÓN ORDENAR
 
+//BOTONES Y SUS EVENTOS
+document.getElementById("personajes").addEventListener("click", () => {
+  showCharacters(dataghibli);
+  filterandsort[0].style.display = "none";
 });
 
-//document.getElementsByClassName("pelicula")[0].style.display = "block";
-//document.getElementsByClassName("pelicula")[1].style.display = "none";
-
-document.getElementById("directores").addEventListener('click', () => { 
-    showDirectors(dataghibli)
-    
+document.getElementById("peliculas").addEventListener("click", () => {
+  showFilms(dataghibli);
+  filterandsort[0].style.display = "flex";
 });
 
+document.getElementById("directores").addEventListener("click", () => {
+  showDirectors(dataghibli);
+  filterandsort[0].style.display = "none";
+});
 
-
-
-
-
-
-
-
-
-
+document.getElementById("inicio").addEventListener("click", () => {
+  sectionAfiche.innerHTML = "";
+  filterandsort[0].style.display = "none";
+});
 
