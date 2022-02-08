fa16e615bc7e4a56955abad46ae78ce2c48a56c1
ceciPino <mepino@uc.cl>
2022-02-08 10:46:00 -0300

    commit x

--- src/main.js
+++ src/main.js
@@ -6,27 +6,23 @@ import data from "./data/ghibli/ghibli.js";
 const dataghibli = data.films;
 
 const sectionAfiche = document.getElementById("root");
+// const sectionDirectores = document.getElementById("dirContainer")
+// const persContainer = document.getElementById("persContainer");
 
-// FUNCION PARA MOSTRAR PERSONAJES
+// PERSONAJES
 const charImage = (characters, name) => {
   return `
 <div>
-<img class="img-personaje" src="${characters}">
-<p class="nombre-personaje">${name}</p>
+<h4 class="nombre-personaje">${name}</h4>
+<img src="${characters}">
 </div>`;
 };
-
-
-
 
 const showCharacters = (dataghibli) => {
   sectionAfiche.innerHTML = "";
   for (let i = 0; i < dataghibli.length; i++) {
     let characters = dataghibli[i].people;
     for (let j = 0; j < characters.length; j++) {
-      // let imageCharacters= characters[j].img;
-      // imageCharacter.setAttribute('class')
-      // imageCharacters.setAttribute('class', 'imgCharacters');
       sectionAfiche.innerHTML += charImage(
         characters[j].img,
         characters[j].name
@@ -35,7 +31,7 @@ const showCharacters = (dataghibli) => {
   }
 };
 
-//FUNCION PARA MOSTRAR PELICULAS
+//UFNCION PARA MOSTRAR PELICULAS
 const showFilms = (dataghibli) => {
   sectionAfiche.innerHTML = "";
   sectionAfiche.style.display = "grid";
@@ -46,7 +42,7 @@ const showFilms = (dataghibli) => {
     let nombre = document.createElement("p");
 
     image.setAttribute("src", dataghibli[i].poster);
-    image.setAttribute("class", "imgfilm");
+    image.setAttribute("class", "imgfilm")
     divImagen.setAttribute("class", "divimagen");
     nombre.setAttribute("class", "titulo");
 
@@ -72,13 +68,14 @@ const showDirectors = (dataghibli) => {
 
     sectionAfiche.appendChild(directors);
 
+    /*let directorsFilt = directors.filter((item,index)=> {
+        return directors.indexOf(item) === index;
+    })*/
   }
 };
 
 // SECCIÓN FILTRAR
-//filtrar peliculas por directores
 const filterandsort = document.getElementsByClassName("filterandsort-section");
-const charactersSection = document.getElementsByClassName("characters-section");
 
 // EL evento change se  usa con elementos "section", "input" y "textarea", su estructura es la sgte:
 const filterSection = document.getElementById("filter-section"); // se declara la variable asociada al fitro y se agrega el evento a esa variable
@@ -86,69 +83,29 @@ filterSection.addEventListener("change", (e) => {
   let filterItem = e.currentTarget.value; //se llama a los valores de los elementos asociados al evento
   // console.log(filterItem)//
   let directorsByFilms = all.filterDirectors(filterItem, dataghibli); //se llaman a todos los elementos dentro de la función creada en data.js
-  showFilms(directorsByFilms); // se muestran las peliculas asociadas a los directores
+  showFilms(directorsByFilms); // se muestran las peliculas asociadas a los directores 
   sectionAfiche.style.display = "flex"; // se muestran las peliculas ordenadas en flex
 });
 
-//filtrar personajes por peliculas
-const characterFilter = document.getElementById("persByFilm");
-characterFilter.addEventListener("change", (e) => {
-  let filterCharacter = e.currentTarget.value;
-  let charactersByFilms = all.filterbyFilms(filterCharacter, dataghibli);
-  showCharacters(charactersByFilms);
-  sectionAfiche.style.display = "flex";
-});
-
-
-/*SECCIÓN ORDENAR
-let dropdownSort = document.getElementById("selectSort");
-dropdownSort.addEventListener('change', function () {
-    let valueSort = dropdownSort.value;
-        if (valueSort === "Newest") { // Ordena por año descendente
-            let infoSorted = sortDataYear(dataghibli);
-            clearPage();
-            mainPoster(infoSorted);
-        }
-        if (valueSort === "Oldest") { // Ordena por año ascendente
-            let OldestYear = sortOldest(dataghibli);
-            clearPage();
-            mainPoster(OldestYear);
-        }
-        if (valueSort === "A_Z") { // Ordena alfabéticamente de A a Z
-            let infoSort = sortAZ(dataghibli);
-            clearPage();
-            mainPoster(infoSort);
-        }
-        if (valueSort === "Z_A") { // Ordena alfabéticamente de Z a A
-            let infoSortR = sortZA(dataghibli);
-            clearPage();
-            mainPoster(infoSortR);
-        }
-});*/
-
+//SECCIÓN ORDENAR
 
 //BOTONES Y SUS EVENTOS
 document.getElementById("personajes").addEventListener("click", () => {
   showCharacters(dataghibli);
   filterandsort[0].style.display = "none";
-  charactersSection[0].style.display = "flex";
-  sectionAfiche.style.display = "grid";
 });
 
 document.getElementById("peliculas").addEventListener("click", () => {
   showFilms(dataghibli);
   filterandsort[0].style.display = "flex";
-  charactersSection[0].style.display = 'none';
 });
 
 document.getElementById("directores").addEventListener("click", () => {
   showDirectors(dataghibli);
   filterandsort[0].style.display = "none";
-  charactersSection[0].style.display = 'none';
 });
 
 document.getElementById("inicio").addEventListener("click", () => {
   sectionAfiche.innerHTML = "";
   filterandsort[0].style.display = "none";
-  charactersSection[0].style.display = 'none';
 });
